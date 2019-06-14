0205b6f5bff21401ad39375c25d4ec6dec23740b
Ygor <emaildoygor@gmail.com>
2019-06-14 16:37:12 -0300

    Commit inicial

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export class Constants {
+    static readonly LARGURA_TOTAL = 220;
+    static readonly ALTURA_TOTAL = 170;
+    static readonly LARGURA_TOTAL_JANELA = Constants.LARGURA_TOTAL + 10;
+    static readonly ALTURA_TOTAL_JANELA = Constants.ALTURA_TOTAL + 10;
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,44 @@
+import { Constants } from './constants';
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(Constants.LARGURA_TOTAL, Constants.ALTURA_TOTAL);
+renderer.domElement.style.display = 'block';
+renderer.domElement.style.margin = '5px auto';
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(createLineGuides());
+scene.add(cube);
+
+camera.position.set( 0, 0, 10 );
+camera.lookAt( 0, 0, 0 );
+
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+animate();
+
+function createLineGuides(): THREE.Line {
+    var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+    geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+    geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+    var line = new THREE.Line(geometry, material);
+    return line;
+}
